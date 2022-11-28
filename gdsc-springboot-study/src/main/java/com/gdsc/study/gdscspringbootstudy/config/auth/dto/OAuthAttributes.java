f258bbe0191f1b60378550e2e9af3275e6df6dae
ChaeyeonHan <kate2236e@naver.com>
2022-11-28 15:54:56 +0900

    [refactor] 유저 권한 수정

--- gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/config/auth/dto/OAuthAttributes.java
+++ gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/config/auth/dto/OAuthAttributes.java
@@ -57,7 +57,7 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
 }
