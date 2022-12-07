f56f65553f5c0b6cadd4b59124e2dc7d6208337f
ChaeyeonHan <kate2236e@naver.com>
2022-12-08 08:58:41 +0900

    [feature] 댓글, 대댓글 삭제

--- gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/web/CommentsApiController.java
+++ gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/web/CommentsApiController.java
@@ -9,25 +9,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("api/v1/comments")
+@RequestMapping("api/v1/comments/{postId}")
 @RestController
 public class CommentsApiController {
 
     private final CommentsService commentsService;
     private final UserRepository userRepository;
 
-    @PostMapping("/{postId}")  // 댓글 작성 api
+    @PostMapping("")  // 댓글 작성 api
     public Long save(@PathVariable Long postId, @RequestBody CommentsSaveRequestDto commentsSaveRequestDto){
-        System.out.println("http://localhost:8080/api/v1/comments/1 요청=================");
+//        System.out.println("http://localhost:8080/api/v1/comments/1 요청=================");
         return commentsService.save(postId, commentsSaveRequestDto);
     }
 
-    @PostMapping("/{postId}/{commentId}/recomment")  // 대댓글 작성 api
+    @PostMapping("{commentId}/recomment")  // 대댓글 작성 api
     public Long save(@PathVariable Long postId, @PathVariable Long commentId, @RequestBody ReCommentsSaveRequestDto reCommentsSaveRequestDto){
         return commentsService.saveRecomment(postId, commentId, reCommentsSaveRequestDto);
     }
 
-    @DeleteMapping("/{postId}/{commentId}")
+
+    @PostMapping("{commentId}")  // 댓글 삭제 api =>
+    public Long deleteComment(@PathVariable Long postId, @PathVariable Long commentId){
+        return commentsService.deleteComment(commentId);
+    }
+
+    @DeleteMapping("{commentId}")  // 대댓글 삭제 api
     public Long delete(@PathVariable Long commentId) {
         commentsService.delete(commentId);
         return commentId;
